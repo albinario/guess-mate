17367d589c30f1619913e746ef4ecd73df4cc709
Albin Lindeborg <albin.lindeborg@gmail.com>
2022-12-01 00:53:04 +0100

    add result to and sort highscore

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const levelEl = document.querySelector('#level');
 const imgEl = document.querySelector('#img-mate');
 const contentEl = document.querySelector('#content');
 const optionsEl = document.querySelector('#options');
@@ -39,15 +40,26 @@ const getName = id => students.find(student => student.id === id).name;
 const showEl = el => el.classList.remove('hide');
 const hideEl = el => el.classList.add('hide');
 
-const highScores = []; // TBD
+const highScores = [{score: 5, dt: '23:45:32'}, {score: 1, dt: '12:00:54'}]; // TBD
 
-const playGame = () => {
-	const rounds = 2;
+const levels = [2, 10, 15, 20, 25, 30, 35, students.length];
+levels.forEach(level => {
+	levelEl.innerHTML += `<button class="btn btn-sm btn-primary my-3">${level}</button>`;
+})
+levelEl.addEventListener('click', e => {
+	if (e.target.tagName === "BUTTON") {
+		levelEl.setAttribute('style', 'display: none;');
+		showEl(imgEl);
+		playGame(Number(e.target.innerText));
+	}
+})
+
+const playGame = level => {
 	let round = 0;
 	let score = 0;	
 	const fails = [];
 
-	roundsEl.innerText = rounds;
+	roundsEl.innerText = level;
 	btnNextEl.innerText = "Next mate";
 
 	shuffleArray(students);
@@ -59,9 +71,7 @@ const playGame = () => {
 
 		const correctId = student.id;
 
-		imgEl.classList.remove('hide');
 		imgEl.setAttribute('src', student.image);
-		// contentEl.innerHTML = '<img src="albin_lindeborg.jpg" id="img-mate" class="img-fluid rounded mx-auto mb-3" alt="Who is this mate?">';
 		optionsEl.innerHTML = '';
 
 		getOptions(correctId).forEach(id => {
@@ -90,25 +100,24 @@ const playGame = () => {
 			}
 		}, { once: true });
 
-		if (round === rounds) { // show different message in btn-next if last round
+		if (round === level) { // show different message in btn-next if last round
 			btnNextEl.innerText = "See result";
 		}
 	}
 
 	btnNextEl.addEventListener('click', e => {
-		if (btnNextEl.innerText === "Next mate") {
+		if (btnNextEl.innerText === "Next mate") { // game is still on
 			playRound(students[round]);
-		} else if(btnNextEl.innerText === "See result") {
+		} else if (btnNextEl.innerText === "See result") { // game is over
 			hideEl(roundCounterEl);
 			hideEl(imgEl);
 			optionsEl.innerText = '';
 			btnNextEl.innerText = "Play again (coming soon...)";
 			contentEl.innerHTML += `
 				<div class="col-xs-12 col-sm-12 card card-body bg-dark mb-1">
-					<div>Your score <span class="badge text-bg-${(score > rounds/2) ? 'success' : 'danger'}">${score}</span></div>
-					<div>Max score <span class="badge text-bg-primary">${rounds}</span></div>
+					<div>Your score <span class="badge text-bg-${(score > level/2) ? 'success' : 'danger'}">${score}</span></div>
+					<div class="small">Max score <span class="badge text-bg-primary">${level}</span></div>
 					<div class="mt-3">💥 ⇩ Highscore ⇩ 💥
-						<p class="small">coming soon...</p>
 						<ol id="high-score"></ol>
 					</div>
 				</div>
@@ -116,10 +125,15 @@ const playGame = () => {
 					<div id="fails" class="row"></div>
 				</div>
 			`;
-			if (highScores.length) { // TBD
-				highScores.forEach(highScore => {
-					document.querySelector('#high-score').innerHTML += ``;
-				})
+			const now = new Date().toLocaleTimeString('se-SE');
+			highScores.push({score: score, dt: now});
+			if (highScores.length) {
+				highScores.sort((a, b) => b.score - a.score);
+				for (i = 0; i < 3; i++) {
+					if (highScores[i]) {
+						document.querySelector('#high-score').innerHTML += `<li class="ml-auto"><span class="badge text-bg-${(highScores[i].score > level/2) ? 'success' : 'danger'}">${highScores[i].score}</span> <span class="small">${highScores[i].dt}</span></li>`;
+					}
+				}
 			}
 			if (fails.length) {
 				fails.forEach(fail => {
@@ -133,19 +147,15 @@ const playGame = () => {
 			} else {
 				document.querySelector('#fails').innerHTML = '<p class="mt-3"><em>No one, you probably already had 🍻 with all.</em></p>'
 			}
-		} else if (btnNextEl.innerText === "Play again") { // TBD
+		} else if (btnNextEl.innerText === "Play again") { // TBD - game should restart
 			// contentEl.innerHTML = '';
 			// showEl(roundCounterEl);
 			// showEl(imgEl);
 			// playGame();
-		} else {
-			btnNextEl.innerText = "Sorry, not yet developed";
+		} else { // fallback
+			btnNextEl.innerText = "Apologies, requested action is not yet developed";
 			btnNextEl.disabled = true;
 		}
 	});
-
 	playRound(students[round]);
-
 }
-
-playGame();
