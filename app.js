0d29fd7a5b25fd2fb72a31fdf3b1894572d60d72
Albin Lindeborg <albin.lindeborg@gmail.com>
2022-11-30 23:10:32 +0100

    add feedback functionality

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
-const mainEl = document.querySelector('main');
 const imgEl = document.querySelector('#img-mate');
+const contentEl = document.querySelector('#content');
 const optionsEl = document.querySelector('#options');
-// const resultsEl = document.querySelector('#results');
 const btnNextEl = document.querySelector('#btn-next');
 
 const roundCounterEl = document.querySelector('#round-counter');
@@ -25,9 +24,9 @@ const shuffleArray = array => {
 const getOptions = id => {
 	let options = [id]; 
 	while (options.length < 4) {
-		let anotherId = getRandomNumber(students.length);
-		if (!options.includes(anotherId)) {
-			options.push(anotherId);
+		let randomId = getRandomNumber(students.length);
+		if (!options.includes(randomId)) {
+			options.push(randomId);
 		}
 	}
 	shuffleArray(options);
@@ -40,97 +39,107 @@ const getName = id => students.find(student => student.id === id).name;
 const showEl = el => el.classList.remove('hide');
 const hideEl = el => el.classList.add('hide');
 
-const rounds = 2;
-let round = 0;
-let score = 0;
-const highScores = [];
-const fails = [];
+const highScores = []; // TBD
 
-roundsEl.innerText = rounds;
-btnNextEl.innerText = "Next mate";
+const playGame = () => {
+	const rounds = 2;
+	let round = 0;
+	let score = 0;	
+	const fails = [];
 
-shuffleArray(students);
+	roundsEl.innerText = rounds;
+	btnNextEl.innerText = "Next mate";
 
-const playRound = student => {
-	round++;
-	btnNextEl.disabled = true;
-	roundEl.innerText = round;
+	shuffleArray(students);
 
-	const correctId = student.id;
+	const playRound = student => {
+		round++;
+		btnNextEl.disabled = true;
+		roundEl.innerText = round;
 
-	imgEl.setAttribute('src', student.image);
+		const correctId = student.id;
 
-	const options = getOptions(correctId);
+		imgEl.setAttribute('src', student.image);
+		optionsEl.innerHTML = '';
 
-	optionsEl.innerHTML = '';
-	options.forEach(id => {
-		optionsEl.innerHTML += `<button class="option btn btn-warning" data-student-id="${id}">${getName(id)}</button>`;
+		getOptions(correctId).forEach(id => {
+			optionsEl.innerHTML += `<button class="option btn btn-warning" data-student-id="${id}">${getName(id)}</button>`;
+		});
+
+		optionsEl.addEventListener('click', e => {
+			if (e.target.tagName === "BUTTON") {
+				document.querySelectorAll('.option').forEach(option => {
+					option.disabled = true;
+				});
+
+				const answer = Number(e.target.dataset.studentId);
+				// console.log("Correct id:", correctId);
+				// console.log("Registered answer:", answer);
+				e.target.classList.remove('btn-warning');
+				if (answer === correctId) {
+					e.target.classList.add('btn-success');
+					score++;
+				} else {
+					e.target.classList.add('btn-danger');
+					fails.push(student);
+				}
+				// console.log("Score:", score);
+				btnNextEl.disabled = false;
+			}
+		}, { once: true });
+
+		if (round === rounds) { // show different message in btn-next if last round
+			btnNextEl.innerText = "See result";
+		}
+	}
+
+	btnNextEl.addEventListener('click', e => {
+		if (btnNextEl.innerText === "Next mate") {
+			playRound(students[round]);
+		} else if(btnNextEl.innerText === "See result") {
+			hideEl(roundCounterEl);
+			hideEl(imgEl);
+			optionsEl.innerText = '';
+			btnNextEl.innerText = "Play again";
+			contentEl.innerHTML += `
+				<div class="col-xs-12 col-sm-12 card card-body bg-dark mb-1">
+					<div>Your score <span class="badge text-bg-${(score > rounds/2) ? 'success' : 'danger'}">${score}</span></div>
+					<div>Max score <span class="badge text-bg-primary">${rounds}</span></div>
+					<div class="mt-3">Highscore 💥<ol id="high-score"></ol></div>
+				</div>
+				<div class="col-xs-12 col-sm-12 card card-body bg-dark">You need to grab a 🍺 with:
+					<div id="fails" class="row"></div>
+				</div>
+			`;
+			if (highScores.length) { // TBD
+				highScores.forEach(highScore => {
+					document.querySelector('#high-score').innerHTML += ``;
+				})
+			}
+			if (fails.length) {
+				fails.forEach(fail => {
+					document.querySelector('#fails').innerHTML += `
+						<div class="col-6">
+							<img src="${fail.image}" class="img-fluid rounded p-2 m-1" alt="${fail.name}">
+							<p class="small">${fail.name}</p>
+						</div>
+					`;
+				});
+			} else {
+				document.querySelector('#fails').innerHTML = '<p class="mt-3"><em>No one, you probably already had 🍻 with all.</em></p>'
+			}
+		} else if (btnNextEl.innerText === "Play again") {
+			contentEl.innerHTML = '';
+			showEl(roundCounterEl);
+			showEl(imgEl);
+			playGame();
+		} else {
+			console.error("Invalid value in btnNextEl.innerText");
+		}
 	});
 
-	optionsEl.addEventListener('click', e => {
-		if (e.target.tagName === "BUTTON") {
-			document.querySelectorAll('.option').forEach(option => {
-				option.disabled = true;
-			});
+	playRound(students[round]);
 
-			const answer = Number(e.target.dataset.studentId);
-			// console.log("Correct id:", correctId);
-			// console.log("Registered answer:", answer);
-			e.target.classList.remove('btn-warning');
-			if (answer === correctId) {
-				e.target.classList.add('btn-success');
-				score++;
-			} else {
-				e.target.classList.add('btn-danger');
-				fails.push(student);
-			}
-			// console.log("Score:", score);
-
-			btnNextEl.disabled = false;
-		}
-	}, { once: true });
-
-	if (round === rounds) { // show different message in button if last round
-		btnNextEl.innerText = 'See result';
-	}
 }
 
-btnNextEl.addEventListener('click', () => {
-	if (round !== rounds) {
-		playRound(students[round]);
-	} else {
-		hideEl(roundCounterEl);
-		hideEl(imgEl);
-		optionsEl.innerText = '';
-		mainEl.innerHTML = `
-			<div class="col-xs-12 col-sm-12 card card-body bg-dark mb-1">
-				<div>Your score <span class="badge text-bg-${(score > rounds/2) ? 'success' : 'danger'}">${score}</span></div>
-				<div>Max score <span class="badge text-bg-primary">${rounds}</span></div>
-				<div class="mt-2">Highscore 💥<ol id="high-score"></ol></div>
-			</div>
-			<div class="col-xs-12 col-sm-12 card card-body bg-dark">You need to grab a 🍺 with:
-				<div id="fails" class="row"></div>
-			</div>
-		`;
-		if (highScores.length) {
-			highScores.forEach(highScore => {
-				document.querySelector('#high-score').innerHTML += ``;
-			})
-		}
-		if (fails.length) {
-			fails.forEach(fail => {
-				document.querySelector('#fails').innerHTML += `
-					<div class="col-6">
-						<img src="${fail.image}" class="img-fluid rounded p-2 m-1" alt="${fail.name}">
-						<p class="small">${fail.name}</p>
-					</div>
-				`;
-			});
-		} else {
-			document.querySelector('#fails').innerHTML = '<div>No one, you probably already had 🍻 with all.</div>'
-		}
-		btnNextEl.classList.add('hide');
-	}
-});
-
-playRound(students[round]);
+playGame();
